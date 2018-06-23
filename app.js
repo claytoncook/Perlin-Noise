d59dee5dab97c36255ce9a22b0b7ad7c3adc936a
Clayton Cook <clayton.cook@muthschools.com>
2018-06-23 03:37:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var canvas = document.querySelector("canvas");
 
 function center() {
-    canvas.style.top = (screen.height/2 - 150) + "px";
-    canvas.style.left = (screen.width/2 - 350) + "px";
+    canvas.style.top = (screen.height/2 - 300) + "px";
+    canvas.style.left = (screen.width/2 - 300) + "px";
 }
 
 center();
